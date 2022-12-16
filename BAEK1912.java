1941037eda3f829b7161681a1db08d9de1dabf6b
ChoiYoonSoo <gews30025@naver.com>
2022-12-16 13:42:36 +0900

    [JAVA] BAEKJOON 1912번 문제풀이
    DP를 이용한 연속합 구하기

--- BAEK1912.java
+++ BAEK1912.java
@@ -1,19 +1,27 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class BAEK1912 {
 
 	public static void main(String[] args) throws IOException {
-		int N;
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
 		
-		N = Integer.parseInt(br.readLine());
+		int []arr = new int[N];
+		int []dp = new int[N];
+		
 		for(int i=0; i<N; i++) {
-			
+			arr[i] = in.nextInt();
 		}
-
+		
+		dp[0] = arr[0];
+		int max = arr[0];
+		
+		for(int i=1; i<N; i++) {
+			dp[i] = Math.max(dp[i-1]+arr[i], arr[i]);
+			max = Math.max(max, dp[i]);
+		}
+		System.out.println(max);
 	}
 
 }
