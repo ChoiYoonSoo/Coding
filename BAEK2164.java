eb27da8a2486c6466d65170dede3a2ced21f442d
ChoiYoonSoo <gews30025@naver.com>
2023-01-08 18:52:44 +0900

    [JAVA] BAEKJOON 2164번 문제풀이
    큐를 이용한 카드2 문제 풀이

---
+++ BAEK2164.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BAEK2164 {
+
+	public static void main(String[] args) {
+		int N;
+		Scanner in = new Scanner(System.in);
+		N = in.nextInt();
+		Queue<Integer> q = new LinkedList<>();
+		
+		for(int i=0; i<N; i++) {
+			q.add(i+1);
+		}
+		
+		while(q.size() > 1) {
+			q.poll();
+			q.add(q.poll());
+		}
+		System.out.println(q.poll());
+
+	}
+
+}
