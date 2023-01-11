b7b2d832672716ea8b1aef8b7c597ef0c10147bf
ChoiYoonSoo <gews30025@naver.com>
2023-01-11 19:17:23 +0900

    [JAVA] BAEKJOON 9655번 문제풀이
    돌 문제 문제풀이 (홀짝)

---
+++ BAEK9655.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BAEK9655 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		
+		if(N%2==0) {
+			System.out.println("CY");
+		}
+		else {
+			System.out.println("SK");
+		}
+
+	}
+
+}
