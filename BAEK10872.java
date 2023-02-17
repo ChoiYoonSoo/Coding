e3839330bec2e2aa5422ac667fdaf85ab69fa787
ChoiYoonSoo <gews30025@naver.com>
2023-02-17 13:43:18 +0900

    [JAVA] BAEKJOON 10872번 문제풀이
    팩토리얼

---
+++ BAEK10872.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BAEK10872 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		int pac = 1;
+		for(int i=1; i<=N; i++) {
+			pac *= i;
+		}
+		
+		
+
+	}
+
+}
